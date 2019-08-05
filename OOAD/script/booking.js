4a9ca3429f644f053ea6580793b6dffc3acf93c1
Nguyen Vuong <vlmnguyen98@gmail.com>
2019-08-05 17:38:48 +0700

    Uploaded Diagrams

---
+++ OOAD/script/booking.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    loadHighRating();
+})
+
+function loadHighRating() {
+    var content = ``;
+    randarr = shuffleArray([...Array(hotels.length).keys()]);
+    randarr.slice(0,3).forEach(function(i) {
+        content += `
+        <div class="row">
+            <div>
+            <a href="roomdetails.html?${hotels[i].ID}"><img class="img-thumbnail" alt="Cinque Terre" width="200" height="80" src="https://www.travelplusstyle.com/wp-content/uploads/2019/01/RafflesMYuzu-Nikkei-Cuisine-cropped.jpg"></a>
+            </div>
+            <div>
+            <a href="roomdetails.html?${hotels[i].ID}"><b>${hotels[i].Name}</b></a>
+            </div>
+            <div>
+            <img src="https://img.icons8.com/wired/24/000000/map.png">
+            <p>${hotels[i].Location}</p>
+            </div>
+        </div>
+        `
+    })
+    $('#highrating').html(content);
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array
+}
