401406d7ce9ae5b112f470a2d3e90b582647cb02
hà <tranthihongha321@gmail.com>
2019-08-06 04:25:02 +0700

    Mat bu ngoc

--- OOAD/script/roomdetails.js
+++ OOAD/script/roomdetails.js
@@ -25,18 +25,27 @@ function showHotelInfo() {
     <span class="btn btn-danger">Bestseller</span>
     <span class="btn btn-primary">Free Wifi</span>
     <br>
-    <br>
     <b>Minimum price: ${Number(Math.min(...hotels[hotelIndex].RoomList.map(room => room.Price))).toLocaleString('en')} VND</b>
     <br>
-    <br>
     <img src="https://img.icons8.com/material-rounded/24/000000/add-user-group-man-man.png"><em>Recommended by ${Math.floor(Math.random()*1000)%(Math.floor(Math.random()*500)+1)+2} people</em>
-    <br>
     <br>
     <img src="https://img.icons8.com/wired/24/000000/map.png">
     <em>${hotels[hotelIndex].Address}</em>
     <br>
     <br>
     <iframe src="https://www.google.com/maps?q=${encodeURIComponent(hotels[hotelIndex].Address)}&output=embed"></iframe>
+     <br>
+         <span style="color: red;">Benefit</span> <br>
+      <img src="https://img.icons8.com/cute-clipart/30/000000/double-tick.png"><em>Free wifi</em> <br>
+      <img src="https://img.icons8.com/cute-clipart/30/000000/double-tick.png"><em>Prevertation cancel policy</em>
+      <br>
+      <span style="color: red;">Other utilitiess</span> <br>
+      <img src="https://img.icons8.com/officel/20/000000/hair-dryer.png"> <i style="font-size: 15px;">      hairdryer</i>   
+      <img style=" padding-left: 30px;" src="https://img.icons8.com/ios/20/000000/air-conditioner.png"><i style="font-size: 15px;">    air conditioner</i> <br>
+      <img src="https://img.icons8.com/officel/16/000000/mirror.png"><i style="font-size: 15px;"> mirror</i> 
+      <img style=" padding-left: 30px;" src="https://img.icons8.com/officel/20/000000/table.png"><i style="font-size: 15px;"> table</i> <br>
+      <img style=" padding-left: 30px;"src="https://img.icons8.com/dusk/20/000000/fridge.png"><i style="font-size: 15px;"> fridge</i> 
+      <img style=" padding-left: 30px;"src="https://img.icons8.com/dusk/20/000000/wardrobe.png"><i style="font-size: 15px;"> wardrobe</i> <br> <br> <br>
     `;
     $("#hotelinfo").html(content);
 }
